86da815d16a3960d3a9903cb9fa2d6d102cf65e5
cbroddle <christenbroddle@gmail.com>
2022-04-08 17:23:40 -0700

    Files created

---
+++ calculator.js
@@ -0,0 +1,33 @@
+//Enter 1rst number
+const a = prompt('Enter first number.');
+
+//Enter 2nd number
+const b = prompt('Enter second number.');
+
+//Choose operator, make into a function
+//const operator = prompt('Enter an operator')
+
+//Function adds 2 numbers 
+function add (a,b) {
+    return a + b;
+}
+
+//Function subtracts 2nd number from 1rst
+function subtract (a,b) {
+    return a - b;
+}
+
+//Func multiples 2 numbers
+function multiply (a,b) {
+    return a * b;
+}
+
+//Func divdes 1rst number by 2nd
+function divide (a,b) {
+    return a/b; 
+}
+
+add();
+subtract();
+multiply();
+divide();

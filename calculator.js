b4a7bf66d1907e0e33f113781affafacefecd640
cbroddle <christenbroddle@gmail.com>
2022-04-19 21:49:36 -0700

    Rework JS event listeners and function to work display

--- calculator.js
+++ calculator.js
@@ -23,48 +23,26 @@ function operate(operator, previousNum, currentNum) {
     
 }
 
-//Show numbers, operators, and answer on calc
-function getDisplay() {
-    //first show previousNum
-    document.getElementByClassName('previousNum').innerText = previousNum;
-    //second show operator
-    document.getElementByClassName('operator').innerText = operator;
-    //next show currentNum
-    document.getElementByClassName('currentNum').innerText = currentNum;
-        //show answer on equals
-
-}
-
-
-const numbers = document.querySelectorAll("buttons.number");
+const numbers = document.querySelectorAll("button.number");
 const previousNum = document.querySelector("div.previousNum"); 
 const operator = document.querySelectorAll("button.operator");
-const currentNum = document.querySelector("div.currentNum"); 
+const currentNum = document.querySelector("currentNum"); 
 const clearNum = document.querySelector("button.clear");
 const deleteNum = document.querySelector("button.delete");
 const equals = document.querySelector("button.equals");
 
+function displayNum(currentNum) {
+    document.getElementById("currentNum").innerText = currentNum;
+}
 
+//click num buttons
 numbers.forEach(button => {
     button.addEventListener('click', () => {
-        //convert value of button to float
-        previousNum = parseFloat(numbers);
-        currentNum = parseFloat(numbers);
-        //store number in previousNum, then next in currentNum
-
+        //store num in a variable
+        currentNum = numbers;
     })
+    displayNum(currentNum);
 })
 
-operator.forEach(button => {
-    button.addEventListener('click', () => {
-        //button value stores value of operator const
-        document.getElementsByClassName('operator').innerText = operator;
-    })
-})
 
-equals.forEach(button => {
-    button.addEventListener('click', () => {
-        //triggers operate function when button pressed
-        operate(operator);
-    })
-})
+
